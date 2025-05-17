ac8e268b06244a36afb8e2f0103df07e11c4ca60
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-05-17 20:23:40 +0300

    js folder update

--- js/task-4.js
+++ js/task-4.js
@@ -1,47 +1,23 @@
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter(user => user.gender === gender)
-    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
-};
+const form = document.querySelector('.login-form');
 
-const clients = [
-  {
-    name: 'Moore Hensley',
-    gender: 'male',
-    balance: 2811,
-  },
-  {
-    name: 'Sharlene Bush',
-    gender: 'female',
-    balance: 3821,
-  },
-  {
-    name: 'Ross Vazquez',
-    gender: 'male',
-    balance: 3793,
-  },
-  {
-    name: 'Elma Head',
-    gender: 'female',
-    balance: 2278,
-  },
-  {
-    name: 'Carey Barr',
-    gender: 'male',
-    balance: 3951,
-  },
-  {
-    name: 'Blackburn Dotson',
-    gender: 'male',
-    balance: 1498,
-  },
-  {
-    name: 'Sheree Anthony',
-    gender: 'female',
-    balance: 2764,
-  },
-];
+form.addEventListener('submit', event => {
+  event.preventDefault(); // ❗ Не дає сторінці перезавантажитися
 
-console.log(getTotalBalanceByGender(clients, 'male'));
+  const { email, password } = form.elements;
 
-console.log(getTotalBalanceByGender(clients, 'female'));
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  form.reset(); // ❗ Очистити всі поля форми
+});
